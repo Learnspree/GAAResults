b8721d3c0994ef47e36ee6ed7a81e181b0511f74
David Jackson <davidjacksonirl@gmail.com>
2026-03-05 21:37:43 +0000

    added aws provider

--- tofu/providers.tf
+++ tofu/providers.tf
@@ -4,4 +4,15 @@ terraform {
     key    = "gaaresults/tofustate"
     region = "us-east-1"
   }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.35.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
